76d74b22ebef04ecdc53ac3871cfbc4b922e8c67
Jędrzej Lewandowski <jedrzejblew@gmail.com>
2019-08-24 12:13:29 +0200

    feat: define SentSMS and PendingSentSMS entities

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,4 @@
 export { Advice } from "./model/advice/Advice";
 export { PendingAdvice } from "./model/advice/PendingAdvice";
+export { SentSMS } from "./model/sentsms/SentSMS";
+export { PendingSentSMS } from "./model/sentsms/PendingSentSMS";
