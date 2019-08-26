bce6ad55677182b34832fc5e2b1f0d3225ef5756
Jędrzej Lewandowski <jedrzejblew@gmail.com>
2019-08-26 23:55:10 +0200

    fix: pass uid to ImportAdviceToUserFunctionAbstractHandler separately for each processed input
    BREAKING CHANGE ImportAdviceToUserFunctionAbstractHandler.handle requires uid as second argument

--- src/functions/import_advice/ImportAdviceToUserFunctionAbstractHandler.ts
+++ src/functions/import_advice/ImportAdviceToUserFunctionAbstractHandler.ts
@@ -1,19 +1,22 @@
 // tslint:disable member-ordering
+import ow from "ow--fork-by-jblew-with-catching";
+
 import { Advice } from "../../model/advice/Advice";
 import { AdviceRepository } from "../../model/advice/AdviceRepository";
 import { Handler } from "../Handler";
 
 import { ImportAdviceToUserFunction as Fn } from "./ImportAdviceToUserFunction";
 
-export abstract class ImportAdviceToUserFunctionAbstractHandler implements Handler<Fn.Function> {
-    protected abstract getAccountUid(): string;
+export abstract class ImportAdviceToUserFunctionAbstractHandler
+    implements Handler<Fn.Function>, Handler<(input: any, props: { uid: string }) => any> {
     protected abstract makeInvalidInputDataError(p: { advanced: string }): Error;
     protected abstract makeAdviceDoesNotExistError(p: { advanced: string }): Error;
     protected abstract makeAdviceAlreadyImportedError(): Error;
     protected abstract getAdviceRepository(): AdviceRepository;
 
-    public async handle(input: { adviceId: string }): Promise<Fn.Result> {
-        const uid = this.getAccountUid();
+    public async handle(input: { adviceId: string }, props: { uid: string }): Promise<Fn.Result> {
+        ow(props.uid, "ImportAdviceToUserFunctionAbstractHandler.props.uid", ow.string.nonEmpty);
+        const uid = props.uid;
         const adviceId = this.getAdviceIdFromInput(input);
         const advice = await this.getAdvice(adviceId);
 
