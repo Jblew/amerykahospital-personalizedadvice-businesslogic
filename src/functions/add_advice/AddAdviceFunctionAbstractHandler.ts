4880c5ba882e57a32726d1e507e7c8dfc79e27a5
Jędrzej Lewandowski <jedrzejblew@gmail.com>
2019-08-27 00:35:21 +0200

    feat: check medicalprofessional role in AddAdviceFunction

--- src/functions/add_advice/AddAdviceFunctionAbstractHandler.ts
+++ src/functions/add_advice/AddAdviceFunctionAbstractHandler.ts
@@ -1,7 +1,10 @@
 // tslint:disable member-ordering
+import ow from "ow--fork-by-jblew-with-catching";
+
 import { Advice } from "../../model/advice/Advice";
 import { AdviceRepository } from "../../model/advice/AdviceRepository";
 import { PendingAdvice } from "../../model/advice/PendingAdvice";
+import { RoleKey } from "../../roles/RoleKey";
 import { Handler } from "../Handler";
 
 import { AddAdviceFunction as Fn } from "./AddAdviceFunction";
@@ -10,9 +13,14 @@ import { AlmostUniqueShortIdGenerator } from "./AlmostUniqueShortIdGenerator";
 export abstract class AddAdviceFunctionAbstractHandler implements Handler<Fn.Function> {
     protected abstract getTimestampSeconds(): number;
     protected abstract makeInvalidInputDataError(p: { advanced: string }): Error;
+    protected abstract makeMissingRoleError(p: { advanced: string }): Error;
     protected abstract getAdviceRepository(): AdviceRepository;
+    protected abstract userHasRole(p: { uid: string; role: string }): Promise<boolean>;
 
-    public async handle(input: PendingAdvice): Promise<Fn.Result> {
+    public async handle(input: PendingAdvice, props: { uid: string }): Promise<Fn.Result> {
+        ow(props.uid, "AddAdviceFunctionAbstractHandler.props.uid", ow.string.nonEmpty);
+        await this.assertUserIsMedicalProfessional(props.uid);
+
         const pendingAdvice = this.inputToPendingAdvice(input);
         const id = await this.obtainUniqueId();
         const advice = this.pendingAdviceToAdvice(pendingAdvice, id);
@@ -44,5 +52,13 @@ export abstract class AddAdviceFunctionAbstractHandler implements Handler<Fn.Fun
     private async obtainUniqueId(): Promise<string> {
         const checkIfAlreadyExists = (id: string) => this.getAdviceRepository().adviceExists(id);
         return AlmostUniqueShortIdGenerator.obtainUniqueId(checkIfAlreadyExists);
+    }
+
+    private async assertUserIsMedicalProfessional(uid: string) {
+        const role = RoleKey.medicalprofessional;
+        const hasMpRole = await this.userHasRole({ uid, role });
+        if (!hasMpRole) {
+            throw this.makeMissingRoleError({ advanced: `Missing role "${role}"` });
+        }
     }
 }
