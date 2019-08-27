b98b7b5830f0e46925b76ce98593f7c67826c39f
Jędrzej Lewandowski <jedrzejblew@gmail.com>
2019-08-27 15:47:38 +0200

    fix: add PendingSentSMS.result validation

--- src/model/sentsms/PendingSentSMS.ts
+++ src/model/sentsms/PendingSentSMS.ts
@@ -3,7 +3,7 @@ import ow from "ow--fork-by-jblew-with-catching";
 export interface PendingSentSMS {
     phoneNumber: string;
     message: string;
-    result: any;
+    result: string | object;
     error?: string;
     test?: boolean;
 }
@@ -13,6 +13,7 @@ export namespace PendingSentSMS {
         ow(s.phoneNumber, `${namePrefix}.phoneNumber`, ow.string.nonEmpty);
         ow(s.message, `${namePrefix}.message`, ow.string.nonEmpty);
         ow(s.error, `${namePrefix}.error`, ow.optional.string.nonEmpty);
+        ow(s.result, `${namePrefix}.result`, ow.optional.any(ow.object, ow.string));
         ow(s.test, `${namePrefix}.test`, ow.optional.boolean);
     }
 }
