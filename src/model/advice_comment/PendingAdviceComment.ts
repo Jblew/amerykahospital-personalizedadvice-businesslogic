701e309393e03f7d94eb20fc18a6bb10bb30f26d
Jędrzej Lewandowski <jedrzejblew@gmail.com>
2019-11-13 01:54:15 +0100

    feat: add AdviceId field to AdviceComment model

--- src/model/advice_comment/PendingAdviceComment.ts
+++ src/model/advice_comment/PendingAdviceComment.ts
@@ -1,17 +1,20 @@
 import ow from "ow--fork-by-jblew-with-catching";
 
 export interface PendingAdviceComment {
+    adviceId: string;
     text: string;
 }
 
 export namespace PendingAdviceComment {
     export function validate(o: PendingAdviceComment, prefix: string = "PendingAdviceComment") {
         ow(o, `${prefix}`, ow.object);
+        ow(o.adviceId, `${prefix}.adviceId`, ow.string.nonEmpty);
         ow(o.text, `${prefix}.text`, ow.string.nonEmpty);
     }
 
     export type KeysType = { [x in keyof Required<PendingAdviceComment>]: string };
     export const keys: KeysType = {
+        adviceId: "adviceId",
         text: "text",
     };
 }
